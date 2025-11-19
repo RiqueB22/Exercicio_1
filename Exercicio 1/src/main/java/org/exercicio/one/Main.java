8b25b339bae259bfe099faadd3dce7674b07bf5f
Rique22 <henrique@cnia.com.br>
2025-11-19 16:06:05 -0300

    Alterando comentarios

--- Exercicio 1/src/main/java/org/exercicio/one/Main.java
+++ Exercicio 1/src/main/java/org/exercicio/one/Main.java
@@ -2,32 +2,33 @@ package org.exercicio.one;
 
 import java.util.Scanner;
 
-//Main class
+//Calcula a soma do numero informado se ele for >= à 1
+
 public class Main {
     public static void main(String[] args) {
 
-        //variables
+        //Variaveis
         int n = 0;
         int total = 0;
         String entrada;
 
-        //Input to number
+        //Input
         Scanner input = new Scanner(System.in);
         System.out.println("Ponha um numero maior ou igual à 1:");
         entrada = input.nextLine();
 
-        //Check if is letter
+        //Checa se tem numeros
         if(entrada.chars().noneMatch(Character::isLetter) && !entrada.isBlank()){
             n = Integer.parseInt(entrada);
         }else{
-            //if is letter, the program close
+            //Se tem letra, fecha o programa
             System.out.println("Digite um numero, não uma letra ou deixe-as vazias.");
-            System.exit(400);
+            System.exit(0);
         }
 
-        //Check if the number is more or equal than 1
+        //Checa se o numero é >= à 1
         if (n >= 1){
-            //Do the addition
+            //Faz adição
             for (int i = 1; i <= n; i++){
                 total += i;
                 System.out.println("Total: " + total);
